e667a29fb010bb5f9c6d486208f04243f5afe43c
mas-fahmi <vantovelgg@gmail.com>
2023-03-20 14:27:47 +0700

    add order controller

--- BackEnd/routes/routes.js
+++ BackEnd/routes/routes.js
@@ -3,4 +3,5 @@ module.exports = function(app){
     //Route Order
     var jsonOrder = require('./../controllers/OrderController.js');
     app.route('/order').get(jsonOrder.index);
+    app.route('/addOrder').post(jsonOrder.addOrder);
 }
