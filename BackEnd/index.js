12a1abd9110c5de063ada8c8b757be4423bbc743
mas-fahmi <vantovelgg@gmail.com>
2023-03-20 13:45:16 +0700

    add routes

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+var routes = require('./routes/routes.js');
 const app = express()
 const port = 3001
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+routes(app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ BackEnd/routes/routes.js
@@ -0,0 +1,6 @@
+module.exports = function(app){
+
+    //Route Order
+    var jsonOrder = require('./../controllers/OrderController.js');
+    app.route('/order').get(jsonOrder.index);
+}
