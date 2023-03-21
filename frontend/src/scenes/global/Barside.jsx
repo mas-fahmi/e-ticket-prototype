e69df70cc23f533b538a1a95874827f778088323
MawlDonald8 <makimuramyu@gmail.com>
2023-03-21 13:54:18 +0700

    Menambahkan Menu Link

--- frontend/src/scenes/global/Barside.jsx
+++ frontend/src/scenes/global/Barside.jsx
@@ -15,7 +15,7 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
     <MenuItem
       active={selected === title}
       style={{ color: colors.grey[100] }}
-      onClick={() => setSelected(title)}
+      // onClick={() => setSelected(title)}
       icon={icon}
     >
       <Typography>{title}</Typography>
