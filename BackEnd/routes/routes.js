10e265b33d467ee341f2a548dcba83a2173d11ee
mas-fahmi <vantovelgg@gmail.com>
2023-03-27 13:30:10 +0700

    update

--- BackEnd/routes/routes.js
+++ BackEnd/routes/routes.js
@@ -1,14 +1,5 @@
 module.exports = function(app){
 
-    //Route Order
-    var jsonOrder = require('./../controllers/OrderController.js');
-    app.route('/order').get(jsonOrder.index);
-    app.route('/addOrder').post(jsonOrder.addOrder);
-    app.route('/showOrder').get(jsonOrder.showOrder);
-    app.route('/showOrder/Detail').get(jsonOrder.showOrderId);
-    app.route('/updateOrder/:id_ticket').put(jsonOrder.updateOrder);
-    app.route('/deleteOrder/:id_ticket').delete(jsonOrder.deleteOrder);
-
     //Route Booking
     var jsonBooking = require('./../controllers/BookingController.js');
     app.route('/booking').get(jsonBooking.index);
