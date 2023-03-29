6dd1eaf810c663db09fce0a13e6a1e21f67d0e40
Haikalmaulanaf <Haikalmfirdaus7@gmail.com>
2023-03-29 15:58:42 +0700

    post get ready

--- andro/E-ticket/src/redux/store.js
+++ andro/E-ticket/src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import dataSlice from "./reducers/dataSlice"
 import userReducer from "./reducers/userSlice";
 import alertReducer from "./reducers/alertSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
-        alert: alertReducer
+        alert: alertReducer,
     }
 });
--- andro/E-ticket/src/screens/dashboard/Ticket.js
+++ andro/E-ticket/src/screens/dashboard/Ticket.js
@@ -1,6 +1,7 @@
-import { StyleSheet, StatusBar, ScrollView, Text, View , Button, Image} from 'react-native';
+import { StyleSheet, StatusBar, ScrollView, Text, View , Button } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { ButtonText, Carousel, ContainerView, ModalLoader } from '../../components';
+import axios from 'axios';
+import {ContainerView, ModalLoader } from '../../components';
 import textStyles from '../../constants/TextStyles';
 //Navigation
 import { useNavigation } from '@react-navigation/native';
@@ -8,6 +9,18 @@ import { ROUTES } from '../../navigations';
 //Redux 
 import { useDispatch, useSelector } from 'react-redux';
 import { GlobalColors } from '../../constants/Styles';
+import { TextInput } from 'react-native-gesture-handler';
+
+const Item = ({nik, name, alamat}) => {
+    return (
+        <View style={styles.itemContainer}>
+            <Text style={styles.itemContainer}>{nik}</Text>
+            <Text style={styles.itemContainer}>{name}</Text>
+            <Text style={styles.itemContainer}>{alamat}</Text>
+            <Text style={styles.delete}>X</Text>
+        </View>
+    )
+}
 
 export default function Ticket() {
     
@@ -20,40 +33,38 @@ export default function Ticket() {
             navigation.navigate(ROUTES.LOGIN)
         }
     }, [dataProfile])
+    
+    const [nik, setNIK] = useState("");
+    const [name, setName] = useState("");
+    const [alamat, setAlamat] = useState("");
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
-        // call api method get
-        // fetch('http://192.168.1.10:3001/showBooking/')
-        // .then(response => response.json())
-        // .then(json => console.log(json))
-
-        // call api method post
-        // const dataForAPI = {
-        //     name: "morpheus",
-        //     job: "leader",
-        // }
-
-        // console.log('data object', dataForAPI);
-        // console.log('data stringify', JSON.stringify(dataForAPI)),
-        // fetch ('https://reqres.in/api/users/ /', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(dataForAPI)
-        // })
-        // .then(response => response.json())
-        // .then(json => {
-        //     console.log('post response: ', json)
-        // })
-
+        getData();  
     }, []);
 
+    const submit = () => {
+        const data = {
+            nik,
+            name,
+            alamat,
+        }
+        axios.post('http://10.200.0.183:3004/users', data)
+        .then(res => {
+            console.log('res : ', res);
+            setNIK("");
+            setName("");
+            setAlamat("");
+            getData();
+        })
+    }
 
     const getData = () => {
-        fetch('http://10.200.0.183:3001/showBooking/')
-        .then(response => response.json())
-        .then(json => console.log(json))
+        axios.get('http://10.200.0.183:3004/users')
+        .then(res => {
+            console.log('res get data: ', res);
+            setUsers(res.data)
+        })
     }
     return (
         <ContainerView>
@@ -61,22 +72,30 @@ export default function Ticket() {
             <ScrollView contentContainerStyle={{ paddingBottom: 80 }} showsVerticalScrollIndicator={false}>
                 <StatusBar hidden={true} />
                 <Text style={[textStyles.textBold15, GlobalColors.white, { marginBottom: 20, marginTop: 50, textAlign: 'center' }]}>Ini Ticket Screen</Text>
-                
-{/* Read */}
-                <Button title="GET DATA" onPress={getData}/>
-                <Text>response GET DATA</Text> 
-                {/* <Image source={{uri: dataUser.avatar}} style={styles.avatar}/>
-                <Text style={[textStyles.textBold15, GlobalColors.white]}>{`${dataUser.first_name} ${dataUser.last_name}`}</Text>
-                <Text style={[textStyles.textMd15, GlobalColors.white]}>{dataUser.email}</Text> */}
-                <View style={styles.line}/>
+                <View style={styles.container}>
+                    <Text style={styles.textTitle}>Local Api (JSON SERVER)</Text>
+                    <Text style={{color: 'white'}}>Masukan Data User</Text>
+                    <TextInput placeholder='NIK' style={styles.input} value={nik} onChangeText={(value) => setNIK(value)}/>
+                    <TextInput placeholder='Nama'  style={styles.input} value={name} onChangeText={(value) => setName(value)}/>
+                    <TextInput placeholder='Alamat'  style={styles.input} value={alamat} onChangeText={(value) => setAlamat(value)}/>
+                    <Button title='Simpan' onPress={submit}/>
+                    <View style={styles.line}/>
+                        {users.map(user => {
+                            return <Item key={user.id} nik={user.nik} name={user.name} alamat={user.alamat}  />
+                        })}
+                </View>
             </ScrollView>
         </ContainerView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {padding: 20},
+    container: {padding: 20, },
     textTitle: {textAlign: 'center'},
     line: {height: 2, backgroundColor: 'black', marginVertical: 20},
-    avatar: {width: 100, height: 100, borderRadius: 100}
+    avatar: {width: 100, height: 100, borderRadius: 100},
+    input: {borderWidth: 1,marginBottom: 12, borderRadius:25, paddingHorizontal: 18, color: 'white', borderColor: 'white'},
+    delete: {fontSize: 20, fontWeight: 'bold', color: 'red',  marginBottom: 15},
+    itemContainer: {color: 'white', fontSize: 18},
+
 })
