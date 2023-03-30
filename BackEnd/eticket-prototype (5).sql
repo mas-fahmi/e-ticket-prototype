db4c098927908116ff3fadea6ee031c0052c5070
mas-fahmi <vantovelgg@gmail.com>
2023-03-30 14:20:00 +0700

    REAL DON DECK!!

--- BackEnd/eticket-prototype (5).sql
+++ BackEnd/eticket-prototype (5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2023 at 08:33 AM
+-- Generation Time: Mar 30, 2023 at 09:19 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.19
 
@@ -72,8 +72,7 @@ INSERT INTO `tb_booking` (`id`, `id_ticket`, `nik`, `name`, `email`, `fest_name`
 (3, 'EciDYuCvQl', '1295120509', 'Babon', 'Babon468@gmail.com', 'Waku Waku', 'Dana', '2023-03-29 03:54:29', 'Blm Bayar'),
 (4, 'zmn9XwRUzE', '1295120509', 'Bobon', 'bobon69@gmail.com', 'Waku Waku', 'Dana', '2023-03-29 03:54:44', 'Blm Bayar'),
 (5, 'nglNUgM000', '1295120509', 'Ratih', 'Ratih00@gmail.com', 'Waku Waku', 'Dana', '2023-03-29 03:55:27', 'Blm Bayar'),
-(6, 'NYeDS1vULU', '1295120509', 'Kaka', 'kaka0o0@gmail.com', 'Waku Waku', 'Dana', '2023-03-29 03:55:49', 'Blm Bayar'),
-(7, 'ompbfkxXmw', '1295120509', 'Xanchi', 'xanchi34@gmail.com', 'Waku Waku', 'Dana', '2023-03-29 03:56:09', 'Blm Bayar');
+(6, 'NYeDS1vULU', '1295120509', 'Kaka', 'kaka0o0@gmail.com', 'Waku Waku', 'Dana', '2023-03-29 03:55:49', 'Blm Bayar');
 
 --
 -- Triggers `tb_booking`
@@ -106,7 +105,7 @@ CREATE TABLE `tb_fest` (
 --
 
 INSERT INTO `tb_fest` (`id`, `fest_name`, `price`, `slot`) VALUES
-(2, 'Waku Waku', 87000, 0);
+(2, 'Waku Waku', 87000, 1);
 
 --
 -- Triggers `tb_fest`
@@ -148,6 +147,29 @@ INSERT INTO `tb_ticket` (`id`, `id_ticket`, `nik`, `name`, `email`, `fest_name`,
 (12, 'NYeDS1vULU', '1295120509', 'Kaka', 'kaka0o0@gmail.com', 'Waku Waku', 'Dana', 'In Valid'),
 (13, 'ompbfkxXmw', '1295120509', 'Xanchi', 'xanchi34@gmail.com', 'Waku Waku', 'Dana', 'In Valid');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `refresh_token` text DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `name`, `email`, `password`, `refresh_token`, `createdAt`, `updatedAt`) VALUES
+(1, 'Jimbeh', 'JimbehJago@gmail.com', '$2b$10$o20hku9CgkJqPhOkymo9WOs6JzYWzutgR7mV4pPblLq/z2nVwj48q', NULL, '2023-03-30 07:10:37', '2023-03-30 07:10:37');
+
 --
 -- Indexes for dumped tables
 --
@@ -156,15 +178,15 @@ INSERT INTO `tb_ticket` (`id`, `id_ticket`, `nik`, `name`, `email`, `fest_name`,
 -- Indexes for table `tb_admin`
 --
 ALTER TABLE `tb_admin`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `tb_booking`
 --
 ALTER TABLE `tb_booking`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_ticket` (`id_ticket`),
-  ADD KEY `nik` (`nik`);
+  ADD UNIQUE KEY `id_ticket_2` (`id_ticket`,`nik`);
 
 --
 -- Indexes for table `tb_fest`
@@ -177,8 +199,17 @@ ALTER TABLE `tb_fest`
 --
 ALTER TABLE `tb_ticket`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_ticket_2` (`id_ticket`,`nik`),
   ADD KEY `id_ticket` (`id_ticket`),
   ADD KEY `nik` (`nik`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_2` (`email`),
+  ADD KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -207,6 +238,12 @@ ALTER TABLE `tb_fest`
 --
 ALTER TABLE `tb_ticket`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
