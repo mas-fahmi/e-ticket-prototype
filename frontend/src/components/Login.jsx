cfd6282021b7addda684c12dd59b08c791e90220
MawlDonald8 <makimuramyu@gmail.com>
2023-04-02 19:30:01 +0700

    DON DEKK

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,59 +1,64 @@
-import React from "react";
 import axios from "axios";
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [msg, setMsg] = useState('');
-    const history = useNavigate();
-
-    const Auth = async (e) => {
-        e.preventDefault();
-        try{
-            await axios.post("http://localhost:3001/loginAdmin", {
-                email:email,
-                password:password
-            });
-            history.push("/dashboard");
-        }catch(error){
-            if(error.response){
-                setMsg(error.response.data.msg);
-                console.log(error);
-            }
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+//   useEffect(() => {
+//     if (localStorage.getItem("user-info")) {
+//       navigate("/");
+//     }
+//   });
+  const Auth = async (e) => {
+    e.preventDefault();
+    try{
+        await axios.post('http://localhost:3001/loginAdmin', {
+            email:email,
+            password:password,
+        });
+        navigate("/dashboard")
+    } catch(error){
+        if(error.response){
+            setMsg(error.response.data.msg)
         }
     }
+  }
+
+//   async function login()
+//   {
+//     console.warn(email,password)
+//     let item={email,password};
+//     let result= await fetch("http://localhost:3001/loginAdmin",{
+        // method: 'POST',
+        // headers:{
+        //     "Content-Type": "application/json",
+        //     "Accept":'application/json'
+        // },
+        // body:JSON.stringify(item)
+//     })
+//     result = await result.json();
+//     localStorage.setItem("user-info",JSON.stringify(result))
+//     navigate("/dashboard")
+//   }
   return (
-    <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-      <div className="hero-body">
-        <div className="container">
-          <div className="columns is-centered">
-            <div className="column is-4-desktop">
-                <form className="box" onSubmit={Auth}>
-                    <p className="has-text-center">{msg}</p>
-                    <div className="field mt-5">
-                        <label className="label">Email or Username</label>
-                        <div className="controls">
-                            <input type="text" className="input" value={email} placeholder="Username" onChange={(e) => setEmail(e.target.value)}/>
-                        </div>
-                    </div>
-                    <div className="field mt-5">
-                        <label className="label">Pasword</label>
-                        <div className="controls">
-                            <input type="password" className="input" value={password} placeholder="*****" onChange={(e) => setPassword(e.target.value)}/>
-                        </div>
-                    </div>
-                    <div className="field mt-5">
-                        <button className="button is-success is-fullwidth">Login</button>
-                    </div>
-                </form>
-            </div>
-          </div>
-        </div>
+    <div>
+      <h2>Login</h2>
+      <div>
+        <form onSubmit={Auth}>
+            <p>{msg}</p>
+        <input type="text" value={email} placeholder="email" onChange={(e) => setEmail(e.target.value)}/>
+        <br />
+        <input type="text" value={password} placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
+        <br />
+        <button /*onClick={login}*/>Login</button>
+        </form>
       </div>
-    </section>
+    </div>
   );
-};
+}
 
 export default Login;
